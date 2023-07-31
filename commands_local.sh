ecde22c40fe28b7763e83a4fb4780c2934c7ed24
NightHawk <michael.le611@gmail.com>
2023-08-01 01:49:06 +0800

    Update commands_local.sh

--- commands_local.sh
+++ commands_local.sh
@@ -3,7 +3,7 @@ conda create --name botbusters python==3.7.1
 conda activate botbusters
 conda install --name botbusters pytorch==1.9.1 torchvision==0.10.1 torchaudio==0.9.1 cudatoolkit=10.2 -c pytorch -y
 conda install --name botbusters pyg -c pyg -c conda-forge -y
-conda install --name botbusters -c huggingface tokenizers -y
+conda install --name botbusters -c huggingface tokenizers==0.10.1 -y
 conda install --name botbusters -c conda-forge transformers==4.4.2 -y
 conda install --name botbusters importlib-metadata -y
 conda install --name botbusters packaging==21.3 -y
