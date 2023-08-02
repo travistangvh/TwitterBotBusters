3cb9393cde60978efbc8e4446b001174eabe59f7
michaellephuoc <michael.lephuoc@mastercard.com>
2023-08-03 01:20:22 +0800

    clean up

--- src/Dehghan/get_tweet.py
+++
@@ -1,5 +0,0 @@
-import json
-
-f=open('/data2/whr/czl/TwiBot22-baselines/datasets/Twibot-22/tweet_0.json','r')
-tweet=json.load(f)
-print()
--- src/template_model/preprocess.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.path.append("..")
-
-from utils import get_data_dir
-
-DATA_DIR = get_data_dir("206")
-
-print(DATA_DIR / "Twibot22")
